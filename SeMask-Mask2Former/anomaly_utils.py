bd5f95b3b7b58d2a548774d4d9d8f7c605b5a8a9
mehdifarhadi97 <mehdi@nematifamilyfundation.onmicrosoft.com>
2024-05-12 14:42:27 +0330

    logsum

--- SeMask-Mask2Former/anomaly_utils.py
+++ SeMask-Mask2Former/anomaly_utils.py
@@ -1,10 +1,13 @@
 import matplotlib.pylab as plt
 import numpy as np
 from sklearn.metrics import roc_curve, auc, average_precision_score
+from easydict import EasyDict as edict
+from typing import Callable
+from tqdm import  tqdm
 
 
-class Metric :
-  def evaluate_ood(self, anomaly_score, ood_gts, verbose=True):
+class Metric:
+    def evaluate_ood(self, anomaly_score, ood_gts, verbose=True):
 
         ood_gts = ood_gts.squeeze()
         anomaly_score = anomaly_score.squeeze()
@@ -38,7 +41,8 @@ class Metric :
         }
 
         return result
-  def calculate_ood_metrics(self, out, label):
+
+    def calculate_ood_metrics(self, out, label):
 
         # fpr, tpr, _ = roc_curve(label, out)
 
@@ -48,7 +52,8 @@ class Metric :
         # fpr = fpr_at_95_tpr(out, label)
 
         return roc_auc, prc_auc, fpr
-  def calculate_auroc(self, conf, gt):
+
+    def calculate_auroc(self, conf, gt):
         fpr, tpr, threshold = roc_curve(gt, conf)
         roc_auc = auc(fpr, tpr)
         fpr_best = 0
@@ -59,7 +64,6 @@ class Metric :
                 break
         # print(k)
         return roc_auc, fpr_best, k
-
 
 
 def show_grays(images, cols=2):
@@ -78,3 +82,84 @@ def show_grays(images, cols=2):
 
 def rescale(image, path):
     plt.imsave(path, image, cmap='gray')
+
+
+class OODEvaluator:
+
+    def __init__(self, model):
+
+        self.model = model
+
+    def calculate_auroc(self, conf, gt):
+        fpr, tpr, threshold = roc_curve(gt, conf)
+        roc_auc = auc(fpr, tpr)
+        fpr_best = 0
+        # print('Started FPR search.')
+        for i, j, k in zip(tpr, fpr, threshold):
+            if i > 0.95:
+                fpr_best = j
+                break
+        # print(k)
+        return roc_auc, fpr_best, k
+
+    def calculate_ood_metrics(self, out, label):
+
+        prc_auc = average_precision_score(label, out)
+        roc_auc, fpr, _ = self.calculate_auroc(out, label)
+        return roc_auc, prc_auc, fpr
+
+    def evaluate_ood(self, anomaly_score, ood_gts, verbose=True):
+
+        ood_gts = ood_gts.squeeze()
+
+        anomaly_score = anomaly_score.squeeze()
+
+        ood_mask = (ood_gts == 1)
+        ind_mask = (ood_gts == 0)
+
+        ood_out = anomaly_score[ood_mask]
+        ind_out = anomaly_score[ind_mask]
+
+        ood_label = np.ones(len(ood_out))
+        ind_label = np.zeros(len(ind_out))
+
+        val_out = np.concatenate((ind_out, ood_out))
+        val_label = np.concatenate((ind_label, ood_label))
+
+        if verbose:
+            print(f"Calculating Metrics for {len(val_out)} Points ...")
+
+        auroc, aupr, fpr = self.calculate_ood_metrics(val_out, val_label)
+
+        if verbose:
+            print(f'Max Logits: AUROC score: {auroc}')
+            print(f'Max Logits: AUPRC score: {aupr}')
+            print(f'Max Logits: FPR@TPR95: {fpr}')
+
+        result = {
+            'auroc': auroc,
+            'aupr': aupr,
+            'fpr95': fpr
+        }
+
+        return result
+
+    def compute_anomaly_scores(
+            self,
+            loader,
+    ):
+
+        anomaly_score = []
+        ood_gts = []
+
+        for image, gt, ignore, file in tqdm(loader, desc="Dataset Iteration"):
+            ood_gts.extend([gt.numpy()])
+
+            score = self.model(image, 0.7)  # -> (H, W)
+
+            anomaly_score.extend([score.cpu().numpy()])
+
+        ood_gts = np.array(ood_gts)
+        anomaly_score = np.array(anomaly_score)
+
+        return anomaly_score, ood_gts
